84531746d492b7b30f82d99a8c5bfedafd6bb45d
marosvolgyimgy <72457174860@szily.hu>
2025-03-03 11:06:38 +0100

    Add files via upload

---
+++ Mobile.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'dbcontroller.php';
+
+Class Mobile
+{
+    private $mobiles=array();
+
+    public function getAllMobile()
+    {
+        $query="Select * from tbl_mobile";
+        $dbcontroller = new DBController();
+        $this->mobiles = $dbcontroller->executeSelectQuery($query);
+        return $this->mobiles;
+    }
+}
+
+
+?>
